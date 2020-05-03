cdd3f209a0a1049581b5cf0de60c2a9333299a8b
kamilah santos <kahsantos89@gmail.com>
2020-05-03 20:11:18 -0300

    estrutura inicial da api

---
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/HeroesApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.livecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApiApplication.class, args);
+	}
+
+}
---
+++ livecoding/src/test/java/com/digitalinnovation/livecoding/HeroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.livecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
