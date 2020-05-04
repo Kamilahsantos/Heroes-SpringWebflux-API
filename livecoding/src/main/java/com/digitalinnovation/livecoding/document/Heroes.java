c1e091be96b9cc015b08a090b165ac305025b53f
kamilah santos <kahsantos89@gmail.com>
2020-05-03 22:00:05 -0300

    add repository

--- livecoding/src/main/java/com/digitalinnovation/livecoding/document/Heroes.java
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/document/Heroes.java
@@ -1,4 +1,72 @@
 package com.digitalinnovation.livecoding.document;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import org.springframework.data.annotation.Id;
+
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
+@NoArgsConstructor
+@DynamoDBTable(tableName ="Heroes_Table")
+
+
 public class Heroes {
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUniverse() {
+    return universe;
+  }
+
+  public void setUniverse(String universe) {
+    this.universe = universe;
+  }
+
+  public int getFilms() {
+    return films;
+  }
+
+  public void setFilms(int films) {
+    this.films = films;
+  }
+
+  @Id
+  @DynamoDBHashKey (attributeName = "id")
+  private String id;
+
+  @DynamoDBAttribute (attributeName = "name")
+  private String name;
+
+  @DynamoDBAttribute (attributeName = "universe")
+  private String universe;
+
+  @DynamoDBAttribute (attributeName = "films")
+  private int films;
+
+  public Heroes(String id, String name, String universe, int films) {
+    this.id = id;
+    this.name = name;
+    this.universe = universe;
+    this.films = films;
+  }
 }
+
--- livecoding/src/main/java/com/digitalinnovation/livecoding/repository/HeroesRepository.java
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/repository/HeroesRepository.java
@@ -1,4 +1,10 @@
 package com.digitalinnovation.livecoding.repository;
 
-public class HeroesRepository {
+import com.digitalinnovation.livecoding.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+
 }
