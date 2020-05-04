f9d67a159cdb5c0165021bfd678f2c3409c76263
kamilah santos <kahsantos89@gmail.com>
2020-05-03 22:35:36 -0300

    criacao da service

--- livecoding/src/main/java/com/digitalinnovation/livecoding/service/HeroesService.java
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/service/HeroesService.java
@@ -1,4 +1,42 @@
 package com.digitalinnovation.livecoding.service;
 
+import com.digitalinnovation.livecoding.document.Heroes;
+import com.digitalinnovation.livecoding.repository.HeroesRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@Service
 public class HeroesService {
+  private final HeroesRepository heroesRepository;
+
+
+  public HeroesService(HeroesRepository heroesRepository) {
+    this.heroesRepository = heroesRepository;
+  }
+
+  public Flux<Heroes> findAll(){
+
+    return Flux.fromIterable(this.heroesRepository.findAll());
+  }
+
+  public  Mono<Heroes> findByIdHero(String id){
+
+    return  Mono.justOrEmpty(this.heroesRepository.findById(id));
+  }
+
+
+  public Mono<Heroes> save(Heroes heroes){
+    return  Mono.justOrEmpty(this.heroesRepository.save(heroes));
+  }
+
+
+  public Mono<Boolean> deletebyIDHero(String id) {
+    heroesRepository.deleteById(id);
+    return Mono.just(true);
+
+  }
+
 }
+
